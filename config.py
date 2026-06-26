6bdcfadd2079aaab9fc5a3626e679a27fb062f4a
jayspatel383 <jayspatel800@gmail.com>
2026-06-26 14:27:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ Configuration file for Crop Disease Early Warning System
 """
 
 # API Configuration
-OPENWEATHER_API_KEY = "you api key"  # 1957925618b635c26a59793ae2d0755d  this is the api key please add in //#
+OPENWEATHER_API_KEY = "you api key"  # 1957925618b635c26a59793ae2d0755d  this is the api key please add in #
 AGROMONITORING_API_KEY = "your api key"   #c1ecb930ebbc8ad836130d33c7bc9dbc this api key for agro monitering #
 
 # Application Settings
